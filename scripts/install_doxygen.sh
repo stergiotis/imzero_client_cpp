5a48f690987c1f255782be809cf50e1d4377239b
Panos Stergiotis <p@stergiotis.ch>
2024-02-23 21:33:01 +0100

    fix: quiet

--- scripts/install_doxygen.sh
+++ scripts/install_doxygen.sh
@@ -3,6 +3,6 @@ mkdir -p "$HOME/Downloads"
 cd "$HOME/Downloads"
 
 if [ ! -d "./doxygen-1.10.0" ]; then
-   wget https://www.doxygen.nl/files/doxygen-1.10.0.linux.bin.tar.gz
-   tar xvfz doxygen-1.10.0.linux.bin.tar.gz
+   wget --quiet "https://www.doxygen.nl/files/doxygen-1.10.0.linux.bin.tar.gz"
+   tar xfz "doxygen-1.10.0.linux.bin.tar.gz"
 fi
