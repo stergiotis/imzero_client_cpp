7e856f7b44518ce8c88c9ed68753ce78a8c0ae52
Panos Stergiotis <p@stergiotis.ch>
2024-05-28 23:37:16 +0200

    fix: build

--- skia/build_skia.sh
+++ skia/build_skia.sh
@@ -12,5 +12,5 @@ python3 tools/git-sync-deps
 #./bin/gn args out/Static --list --short
 ./bin/gn gen out/Static --args="is_official_build=false cc=\"clang\" cxx=\"clang++\" is_debug=false is_component_build=false skia_use_gl=true skia_use_system_expat=false skia_use_system_freetype2=false skia_use_system_harfbuzz=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false"
 
-ninja -v -d keeprsp -C out/Static
+./third_party/ninja/ninja -v -d keeprsp -C out/Static
 #./out/Static/viewer
