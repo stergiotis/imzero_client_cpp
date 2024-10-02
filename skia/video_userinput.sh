f32e529b90a07c1aa0bc9770784a1b2430be57dd
Panos Stergiotis <p@stergiotis.ch>
2024-10-02 16:10:22 +0200

    wip: video direct rendering

--- skia/video_userinput.sh
+++ skia/video_userinput.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -v
 rm -f transferRawFrames
 rm -f transferUserInteractionEvents
 mkfifo transferRawFrames
@@ -26,16 +26,20 @@ fps=60
 
 
 pid=$!
-ffmpeg -hide_banner \
-       -re -fflags +genpts  \
+../../contrib/FFmpeg/ffplay -hide_banner \
        -framerate $fps \
        -f image2pipe -vcodec bmp -i transferRawFrames \
-       -flags +global_header -r 30000/1001 \
-       -an \
-       -vaapi_device /dev/dri/renderD128 \
-       -vf "format=nv12,hwupload,scale_vaapi=w=$resW:h=$resH" \
-       -c:v mpeg2_vaapi -qp:v 5 -bf 0  \
-       -f nut "pipe:1" | \
+       -imzero_user_interaction_path transferUserInteractionEvents
+#ffmpeg -hide_banner \
+#       -re -fflags +genpts  \
+#       -framerate $fps \
+#       -f image2pipe -vcodec bmp -i transferRawFrames \
+#       -flags +global_header -r 30000/1001 \
+#       -an \
+#       -vaapi_device /dev/dri/renderD128 \
+#       -vf "format=nv12,hwupload,scale_vaapi=w=$resW:h=$resH" \
+#       -c:v mpeg2_vaapi -qp:v 5 -bf 0  \
+#       -f nut "pipe:1" | \
 #ffmpeg -hide_banner \
 #       -re -fflags +genpts  \
 #       -framerate $fps \
@@ -70,11 +74,11 @@ ffmpeg -hide_banner \
 #       -vf "fps=$fps,format=nv12,hwupload,scale_vaapi=w=$resW:h=$resH" \
 #       -c:v h264_vaapi -qp:v 26 -bf 0 -qp:v 26 -bf 0 -async_depth 4  \
 #       -f nut "pipe:1" | \
-../../contrib/FFmpeg/ffplay -hide_banner \
-       -threads 1 -filter_threads 1 \
-       -probesize 32 -sync ext \
-       -fflags nobuffer -flags low_delay \
-       -fpsprobesize 0 -framedrop -fast -infbuf \
-       -f nut -fflags '+nobuffer' -flags2 '+fast' -i "pipe:0"  \
-       -imzero_user_interaction_path transferUserInteractionEvents
+#../../contrib/FFmpeg/ffplay -hide_banner \
+#       -threads 1 -filter_threads 1 \
+#       -probesize 32 -sync ext \
+#       -fflags nobuffer -flags low_delay \
+#       -fpsprobesize 0 -framedrop -fast -infbuf \
+#       -f nut -fflags '+nobuffer' -flags2 '+fast' -i "pipe:0"  \
+#       -imzero_user_interaction_path transferUserInteractionEvents
 kill $pid
