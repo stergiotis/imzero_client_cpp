dde441a5c7f273b9f802448696ef0a1b35373f5d
Panos Stergiotis <p@stergiotis.ch>
2024-10-19 20:48:30 +0200

    fix: minor fixes and improvements

--- scripts/install_clang.sh
+++ scripts/install_clang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-url="https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-x86_64-linux-gnu-ubuntu-18.04.tar.xz"
+url="https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/"
 fn="clang+llvm-18.1.8-x86_64-linux-gnu-ubuntu-18.04"
 mkdir -p "$HOME/Downloads"
 cd "$HOME/Downloads"
