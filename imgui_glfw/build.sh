d14c94c43f0a38fa62f55858d7b952d6bab2e7f2
Steam Deck User <deck@steamdeck.fritz.box>
2025-02-07 21:34:57 +0200

    fix: build

--- imgui_glfw/build.sh
+++ imgui_glfw/build.sh
@@ -2,6 +2,8 @@
 set -ev
 here=$(dirname "$(readlink -f "$BASH_SOURCE")")
 cd "$here"
+export IMZERO_CLIENT_CPP_ROOT=$(realpath $here)
+
 ../common/generate_links.sh
 ./generate_links.sh
 ./build_cpp.sh
