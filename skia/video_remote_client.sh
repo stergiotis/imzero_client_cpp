e8973aa0d11cfb7466820de07908d55e1d21a48d
Panos Stergiotis <p@stergiotis.ch>
2025-02-08 18:08:18 +0100

    wip: video

--- skia/video_remote_client.sh
+++ skia/video_remote_client.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ev
 
-ffplay zmq:tcp://127.0.0.1:5555
-#ffplay -hide_banner \
-#       -threads 1 -filter_threads 1 \
-#       -probesize 32 -sync ext \
-#       -fpsprobesize 0 -framedrop -fast -infbuf \
-#       -vf "drawtext=text='%{localtime\:%S-%6N}':fontsize=144:box=1:boxcolor=black:fontcolor=red:y=(main_h/2)+text_h" \
-#       -f nut -fflags '+nobuffer' -flags2 '+fast' -i zmq:tcp://127.0.0.1:5555
+../../contrib/FFmpeg/ffplay -hide_banner \
+       -threads 1 -filter_threads 1 \
+       -probesize 32 -sync ext \
+       -fpsprobesize 0 -framedrop -fast -infbuf \
+       -imzero_user_interaction_path transferUserInteractionEvents \
+       -f nut -fflags '+nobuffer' -flags2 '+fast' -i "srt://195.201.119.88:5000?mode=caller&smoother=live"
