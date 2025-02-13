48c066bb8fd8a2568d18a25905b63fad7574f8a1
stergiotis <p@stergiotis.ch>
2025-02-13 23:44:24 +0200

    fix: fixes

--- scripts/nonportable/steamos_3.6.sh
+++ scripts/nonportable/steamos_3.6.sh
@@ -2,29 +2,21 @@
 sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
 # reinstall missing system files
 missing=$(pacman -Qk 2>/dev/null | grep -ve ' 0 missing' | grep -ie ^libc -e glibc -e gcc -e clang -e headers -e udev -e systemd  | cut -d ":" -f 1)
 sudo pacman -Syu "$missing" --overwrite '*'
+sudo pacman -Syu linux-api-headers # linux-api-headers is not correctly reporting missing files
 
 pacman="sudo pacman -S --noconfirm"
-if ! [ -x "$(command -v wget)" ]; then
-	$pacman wget
-fi
-if ! [ -x "$(command -v cmake)" ]; then
-	$pacman cmake
-fi
-if ! [ -x "$(command -v clang)" ]; then
-	$pacman clang
-fi
-if ! [ -x "$(command -v gcc)" ]; then
-	$pacman gcc
-fi
-if ! [ -x "$(command -v xsltproc)" ]; then
-	$pacman libxslt
-fi
-if ! [ -x "$(command -v bzip2)" ]; then
-	$pacman bzip2
-fi
+$pacman wget
+$pacman cmake
+$pacman holo-3.6/clang
+$pacman holo-3.6/gcc
+$pacman libxslt
+$pacman bzip2
+$pacman dhall dhall-json dhall
+
 # freetype
 $pacman freetype2
 
@@ -32,17 +24,19 @@ $pacman freetype2
 $pacman alsa-utils libxext libxcb libdecor wayland
 
 # glfw
-$pacman gflw
+$pacman glfw-x11
 
 $pacman base-devel
 
 # skia dependencies
-$pacman libjpeg-turbo libpng libwebp ninja procps-ng python rsync harfbuzz glibc gcc-libs icu fontconfig expat libc++ libglvnd libx11 xorgproto zlib
+$pacman libjpeg-turbo libpng libwebp ninja procps-ng python rsync harfbuzz gcc-libs icu fontconfig expat libc++ libglvnd libx11 xorgproto zlib
+$pacman holo-3.6/glibc
 
 # note: steam-os uses a non-stock kernel: s/linux-headers/linux-neptune-headers/
-$pacman holo-3.6/linux-headers linux-neptune-headers holo-3.6/linux-lts-headers
+kernel_version=$(uname -a | grep -E -o "neptune-[0-9]+")
+$pacman holo-3.6/linux-headers "linux-${kernel_version}-headers" holo-3.6/linux-lts-headers
 
 # tracy dependencies
-$pacman wayland wayland-protocols libglvnd libxkbcommon freetype2 dbus hicolor-icon-theme capstone libffi brotli holo-3.6/glib2 graphite #intel-tbb
+$pacman wayland wayland-protocols libglvnd libxkbcommon freetype2 dbus hicolor-icon-theme capstone libffi brotli graphite #intel-tbb
 # tracy missing files on steamos
 $pacman brotli holo-3.6/glib2 graphite pcre2 libsysprof-capture libxau libxdmcp
