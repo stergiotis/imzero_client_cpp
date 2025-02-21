f8cc6f3b7afb6c45982c848ed9b3f9af1d43441b
stergiotis <p@stergiotis.ch>
2025-02-22 00:20:36 +0200

    feat: use bgra packing (faster on little endian machines)

--- skia/imgui/imconfig.h
+++ skia/imgui/imconfig.h
@@ -55,7 +55,7 @@
 //#define IMGUI_USER_H_FILENAME         "my_folder/my_imgui_user.h"
 
 //---- Pack colors to BGRA8 instead of RGBA8 (to avoid converting from one to another)
-//#define IMGUI_USE_BGRA_PACKED_COLOR
+#define IMGUI_USE_BGRA_PACKED_COLOR
 
 //---- Use 32-bit for ImWchar (default is 16-bit) to support unicode planes 1-16. (e.g. point beyond 0xFFFF like emoticons, dingbats, symbols, shapes, ancient languages, etc...)
 #define IMGUI_USE_WCHAR32
