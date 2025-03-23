e42d3d7536bfca5f3699859b3222d0ac66e92dae
Panos Stergiotis <p@stergiotis.ch>
2025-03-23 14:27:38 -0700

    fix: windows run fixes

--- skia/build_windows.sh
+++ skia/build_windows.sh
@@ -7,7 +7,7 @@ export IMZERO_CLIENT_CPP_ROOT="$(realpath "$here")"
 export IMGUI_SKIA_CPP_ROOT="$(realpath "$here/..")"
 #./copy_libs_windows.sh
 ./build_cpp_windows.sh
-#./build_go.sh
+./build_go_windows.sh
 rm -f bin/*.exe
 mkdir -p bin
 mv build/imzeroClientSkiaSdl3Impl.exe bin/imgui_skia.exe
--- skia/run_windows.sh
+++ skia/run_windows.sh
@@ -2,9 +2,9 @@
 set -ev
 here=$(dirname "$(readlink -f "$BASH_SOURCE")")
 cd "$here"
-./build_go.sh
+./build_go_windows.sh
 font="./SauceCodeProNerdFontPropo-Regular.ttf"
 VSYNC="${VSYNC:-on}"
-./main_go --logFormat console --httpServerAddress localhost:8888 demo --imGuiBinary ./bin/imgui_skia.exe --mainFontTTF "$font" --mainFontSizeInPixels 13 \
+./main_go.exe --logFormat console --httpServerAddress localhost:8888 demo --imGuiBinary ./bin/imgui_skia.exe --mainFontTTF "$font" --mainFontSizeInPixels 13 \
 	  -clientFontManager "directwrite" -clientSkiaBackendType gl -clientVsync $VSYNC -clientTtfFilePath "$font" \
 	  -clientBackdropFilter off -clientVectorCmd on -clientImguiNavKeyboard on
