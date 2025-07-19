9e65cc9985ecd7550d12d76781d682322a7bfd75
stergiotis <p@stergiotis.ch>
2025-07-19 22:37:09 +0200

    feat: tune default font params

--- skia/run.sh
+++ skia/run.sh
@@ -6,6 +6,6 @@ cd "$here"
 font="./SauceCodeProNerdFontMono-Regular.ttf"
 font=$(../scripts/find_ttf_font_file.sh "DejaVu Sans:style=Book")
 VSYNC="${VSYNC:-on}"
-./main_go --logFormat console --httpServerAddress localhost:8888 demo --imGuiBinary ./bin/imgui_skia_exe --mainFontTTF "$font" --mainFontSizeInPixels 13 \
+./main_go --logFormat console --pprofHttpListenAddress localhost:8888 demo --imGuiBinary ./bin/imgui_skia_exe --mainFontTTF "$font" --mainFontSizeInPixels 13 \
 	  -clientFontManager fontconfig -clientSkiaBackendType gl -clientVsync $VSYNC -clientTtfFilePath "$font" \
 	  -clientBackdropFilter off -clientVectorCmd on -clientImguiNavKeyboard on
--- skia/run_pipe.sh
+++ skia/run_pipe.sh
@@ -10,8 +10,17 @@ font="./martian/MartianMono-StdRg.ttf"
 font="./SauceCodeProNerdFontMono-Regular.ttf"
 font=$(../scripts/find_ttf_font_file.sh "DejaVu Sans:style=Book")
 VSYNC="${VSYNC:-on}"
-./main_go --logFormat console demo --mainFontTTF "$font" --mainFontSizeInPixels 13 "$@" < transfer | \
-	./bin/imgui_skia_exe -ttfFilePath "$font" -fffiInterpreter on -skiaBackendType gl -vsync $VSYNC -backdropFilter off \
-	            -vectorCmd on \
-		    -fontManager fontconfig -imguiNavKeyboard on > transfer
+./main_go --logFormat console demo --mainFontTTF "$font" --mainFontSizeInPixels 15 "$@" < transfer | \
+    ./bin/imgui_skia_exe -ttfFilePath "$font" \
+                         -coreDump off \
+                         -backgroundColorRGBA 8f8f8fff \
+                         -fontDyFudge -0.058 \
+                         -fontScaleOverride 0.822 \
+                         -fffiInterpreter on \
+                         -skiaBackendType gl \
+                         -vsync $VSYNC \
+                         -backdropFilter off \
+                         -fontManager fontconfig \
+                         -imguiNavKeyboard on \
+                         -fullscreen off > transfer
 rm -f transfer
